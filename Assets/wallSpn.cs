ed0e627732a6ee316baf6ffe6ceab49e7ec24144
rlambert6696 <rlambert6696@student.pps.net>
2024-03-13 09:49:58 -0700

    Hello World

--- Assets/wallSpn.cs
+++ Assets/wallSpn.cs
@@ -5,35 +5,33 @@ using UnityEngine;
 public class wallSpawner : MonoBehaviour
 {
     public GameObject wall;
-    public float spawnRate = 3;
-    private float timer = 0;
+    public float spawnRate = 500;
     public float heightOffset = 10;
     public float moveSpeed = 5;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        createWall();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer < spawnRate)
-        {
-            timer += Time.deltaTime;
-        }
-        else
+        if (Random.Range(1, 1000) == 1)
         {
             createWall();
-            timer = 0;
         }
     }
+
     void createWall()
     {
         float lowestPoint = transform.position.y - heightOffset;
         float highestPoint = transform.position.y + heightOffset;
         Instantiate(wall, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
 
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        //transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
     }
 }
